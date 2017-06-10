ebe5c9fc4f092d6e1a6e25767ecc4c5740e6de27
Winter Roberts <wr.paratekk@gmail.com>
2017-06-09 18:41:24 -0700

    Removed an unused import.
    Just cleaning up code, still version 1.5.1

--- src/net/aionstudios/sorttest/SortingAlgorithm.java
+++ src/net/aionstudios/sorttest/SortingAlgorithm.java
@@ -1,7 +1,6 @@
 package net.aionstudios.sorttest;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
